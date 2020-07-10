65b55640f11cd123f3360009c8a2b0aee4a38078
GameGlitz <50027352+GameGlitz@users.noreply.github.com>
2020-07-10 22:36:07 +0100

    RGB fixed my FPS issues
    RGB

--- Beep.bat
+++ Beep.bat
@@ -1,4 +1,41 @@
 @echo off
 title Beep
+color 0a
+cls
+echo ########################################
+echo Welcome to the Beep machine!
 echo Press any key to beep, press 0 to close.
+echo ########################################
 choice /c 0 /n
+:rgbisthebest
+color 1
+echo No mercy rbuh
+color 2
+echo No mercy rbuh
+color 3
+echo No mercy rbuh
+color 4
+echo No mercy rbuh
+color 5
+echo No mercy rbuh
+color 6
+echo No mercy rbuh
+color 7
+echo No mercy rbuh
+color 8
+echo No mercy rbuh
+color 9
+echo No mercy rbuh
+color A
+echo No mercy rbuh
+color B
+echo No mercy rbuh
+color C
+echo No mercy rbuh
+color D
+echo No mercy rbuh
+color E
+echo No mercy rbuh
+color F
+echo No mercy rbuh
+goto :rgbisthebest
